76f3962e3a1ba2be4f0bdfec3b0126021f24af19
A Srinibash Achary <126653603+asrinibash@users.noreply.github.com>
2024-06-24 16:40:16 +0530

    Added animation to the navbar and make  it responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,32 @@
 import React, { useState } from "react";
-import { images } from "../../constants";
+import { images } from "../../Constants";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
   const links = [
     { name: "Home", link: "/" },
-    { name: "About", link: "/about" },
     { name: "Stakeholders", link: "/stakeholder" },
-    { name: "News", link: "/news" },
+    {
+      name: "Training & Assessment",
+      link: "/skillportal",
+      submenu: [
+        "Training Partner Login",
+        "Assessment Partner Login",
+        "Certifications",
+      ],
+    },
+    { name: "Administrative", link: "/contact" },
     { name: "Resources", link: "/resource" },
-    { name: "Training & Assessment", link: "/skillportal" },
-    { name: "Contact us", link: "/contact" },
+    { name: "Notifications", link: "/news" },
   ];
 
   const [open, setOpen] = useState(false);
+  const [activeSubmenu, setActiveSubmenu] = useState(null);
+
+  const handleSubmenuClick = (submenuItem) => {
+    setActiveSubmenu(submenuItem);
+  };
 
   return (
     <>
@@ -49,63 +63,97 @@ function Navbar() {
           </a>
         </div>
       </header>
-      <nav className="shadow-md w-full border border-[#001d3d] top-0 left-0">
-        <div className="flex items-center justify-between bg-[#001d3d] py-4 md:px-10 px-7">
-          <div className="flex-grow md:flex-grow-0">
-            <div
-              onClick={() => setOpen(!open)}
-              className="absolute right-8 top-6 cursor-pointer md:hidden w-7 h-7"
-            >
-              {open ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-7 w-7 text-white"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-7 w-7 text-white"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M3 5a1 1 0 011-1h12a1 1 0 010 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 010 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 010 2H4a1 1 0 01-1-1z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              )}
-            </div>
-          </div>
-          <ul
-            className={`flex-grow flex flex-col md:flex-row md:items-center md:justify-end md:space-x-10 md:space-y-0 space-y-7 md:static absolute w-full left-0 bg-[#001d3d] md:bg-transparent transition-all duration-500 ease-in ${
-              open ? "top-16" : "top-[-490px]"
-            }`}
-          >
-            {links.map((link) => (
-              <li key={link.name} className="text-lg">
-                <a
-                  href={link.link}
-                  className="text-white hover:text-gray-400 duration-500"
-                >
-                  {link.name}
-                </a>
-              </li>
-            ))}
-            <li>
-              <button className="btn bg-[#0066ff] text-lg text-white font-semibold px-3 py-1 rounded duration-500 hover:bg-[#3f37c9]">
-                Get Started
+      <nav className="sticky top-0 shadow-md w-full border border-[#001d3d] z-50 bg-gray-900">
+        <div className="container mx-4 px-4">
+          <div className="flex items-center justify-end py-4 relative gap-8">
+            <div className="md:hidden">
+              <button
+                onClick={() => setOpen(!open)}
+                className="text-white focus:outline-none"
+              >
+                {open ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                )}
               </button>
-            </li>
-          </ul>
+            </div>
+            <ul
+              className={`md:flex md:items-center md:space-x-8 ${
+                open
+                  ? "block absolute top-full left-0 w-full bg-gray-900 p-4"
+                  : "hidden"
+              } md:static md:w-auto md:bg-transparent md:p-0`}
+            >
+              {links.map((link) => (
+                <li
+                  key={link.name}
+                  className="menu-item text-base relative group py-2 md:py-0"
+                >
+                  <a
+                    href={link.link}
+                    className="text-white hover:text-gray-300 transition duration-300"
+                  >
+                    {link.name}
+                  </a>
+                  {link.submenu && (
+                    <ul className="submenu md:absolute md:bg-white md:shadow-lg md:rounded-base md:mt-2">
+                      {link.submenu.map((subItem, index) => (
+                        <li
+                          key={index}
+                          className={`md:px-4 md:py-2 ${
+                            activeSubmenu === subItem ? "bg-gray-200" : ""
+                          }`}
+                          onClick={() => handleSubmenuClick(subItem)}
+                        >
+                          <a
+                            href="#"
+                            className="text-black hover:text-[#0066ff] block"
+                          >
+                            {subItem}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </li>
+              ))}
+              <li className="md:hidden mt-4">
+                <Link to="/contact">
+                  <button className="btn bg-[#0066ff] text-base text-white font-semibold px-3 py-1 rounded duration-500 hover:bg-[#3f37c9] w-full">
+                    Contact Us
+                  </button>
+                </Link>
+              </li>
+            </ul>
+            <Link to="/contact" className="hidden md:block">
+              <button className="btn bg-[#0066ff] text-base text-white font-semibold px-3 py-1 rounded duration-500 hover:bg-[#3f37c9]">
+                Contact Us
+              </button>
+            </Link>
+          </div>
         </div>
       </nav>
     </>
